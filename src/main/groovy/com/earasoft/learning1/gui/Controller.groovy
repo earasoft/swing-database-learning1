56e86fac399283428b1e9b6b9ca9d0c92bc8bd65
Manny Rivera <mcomp2010@yahoo.com>
2014-11-18 12:55:45 -0500

    Added Swing Workers

--- src/main/groovy/com/earasoft/learning1/gui/Controller.groovy
+++ src/main/groovy/com/earasoft/learning1/gui/Controller.groovy
@@ -1,5 +1,10 @@
 package com.earasoft.learning1.gui;
 
+import java.io.IOException;
+import java.sql.SQLException
+
+import javax.swing.SwingWorker;
+
 import org.apache.commons.configuration.PropertiesConfiguration;
 
 import com.earasoft.db.dao.People;
@@ -11,37 +16,83 @@ import com.earasoft.db.database.manager.DatabaseManagerImpl;
 
 public class Controller {
     private ViewBind view;
+    PropertiesConfiguration propertiesConfiguration
+    DatabaseManagerImpl DbMgrDemo
     
     public Controller(GuiMain view){
         this.view = new ViewBind(view);
+        
     }
     
     public init(){
-		
-		PropertiesConfiguration propertiesConfiguration = new PropertiesConfiguration("config/settings.properties");
-		
-		DatabaseManagerImpl DbMgrDemo = DatabaseManagerBuilder.getDatabaseManager(propertiesConfiguration);
 		//DbMgrDemo.clearDatabase();
-		DbMgrDemo.openDatabase();
-		
-		People personDAO = new PeopleDAO(DbMgrDemo);
-		
-		//System.out.println(personDAO.checkIfPersonExist("1549490016"));
-		
-		//personDAO.addPerson(new PersonDAO("first","R4","445-555-4446"));
-		for(int i = 1; i<=1000; i++){
-			personDAO.addPerson(new PersonDAO("f54","R4","555-555-4444"));
-			System.out.println("person: " + i);
-		}
+        println "ke"
+        
+        propertiesConfiguration = new PropertiesConfiguration("config/settings.properties");
+        DbMgrDemo = DatabaseManagerBuilder.getDatabaseManager(propertiesConfiguration);
+        
+        SwingWorker<Boolean, Void> worker1 = new SwingWorker<Boolean, Void>() {
+            public Boolean doInBackground() throws SQLException {
+                DbMgrDemo.openDatabase();
+                return true
+            }
+            
+            public void done() {
+                try {
+                    def doc = get();
+                    System.out.println(doc);
+                    //display(doc);
 
-		for(Person person : personDAO.getPeople()){
-			Models.getListModel().addElement(person);
-		}
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+        worker1.execute();
+        
+        
+        
 		
+        println "he"
 		
-	
+        init_people()
     }
 	
+    public void init_people(){
+        if(DbMgrDemo.currentConnection == null)
+            return
+        People personDAO = new PeopleDAO(DbMgrDemo);
+        
+        //System.out.println(personDAO.checkIfPersonExist("1549490016"));
+        
+        //personDAO.addPerson(new PersonDAO("first","R4","445-555-4446"));
+//      for(int i = 1; i<=1000; i++){
+//          personDAO.addPerson(new PersonDAO("f54","R4","555-555-4444"));
+//          System.out.println("person: " + i);
+//      }
+
+        SwingWorker<List<Person>, Void> worker = new SwingWorker<List<Person>, Void>() {
+            public List<Person> doInBackground() throws IOException {
+                return personDAO.getPeople(); // heavy task
+            }
+            
+            public void done() {
+                try {
+                    List<Person> doc = get();
+                    System.out.println(doc.size());
+                    //display(doc);
+                    
+                    for(Person person : doc){
+                        Models.getListModel().addElement(person);
+                    }
+                    
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+        worker.execute();
+    }
 	public void exit(){
 		System.exit(0);
 	}
