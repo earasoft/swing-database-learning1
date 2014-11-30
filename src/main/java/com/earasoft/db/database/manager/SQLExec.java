e225e3bb7240fa578f939106a356f6c7ff2c7d27
Chris Bosma <cmbosma@gmail.com>
2014-11-30 18:18:31 -0500

    tables

--- src/main/java/com/earasoft/db/database/manager/SQLExec.java
+++ src/main/java/com/earasoft/db/database/manager/SQLExec.java
@@ -18,20 +18,35 @@ public class SQLExec {
 	}
 	
 	static class Sqlite{
-		public static final String CREATE_PERSON_TABLE = "CREATE TABLE IF NOT EXISTS person (personId INTEGER PRIMARY KEY AUTOINCREMENT, firstName varchar(100), lastName varchar(255), phoneNumber varchar(32));";
-		public static final String CREATE_PERSON_INDEX_LAST_NAME = "CREATE INDEX personByLastName ON person (lastName ASC);";
+//		public static final String CREATE_PERSON_TABLE = "CREATE TABLE IF NOT EXISTS person (personId INTEGER PRIMARY KEY AUTOINCREMENT, firstName varchar(100), lastName varchar(255), phoneNumber varchar(32));";
+//		public static final String CREATE_PERSON_INDEX_LAST_NAME = "CREATE INDEX personByLastName ON person (lastName ASC);";
 		
-		public static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS user (userId INTEGER PRIMARY KEY AUTOINCREMENT, username varchar(100), password varchar(255), role varchar(32));";
-		public static final String CREATE_USER_INDEX_USERNAME = "CREATE INDEX userByUsername ON user (username ASC);";
+//		public static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS user (userId INTEGER PRIMARY KEY AUTOINCREMENT, username varchar(100), password varchar(255), role varchar(32));";
+//		public static final String CREATE_USER_INDEX_USERNAME = "CREATE INDEX userByUsername ON user (username ASC);";
+		
+//		public static final String CREATE_TIMESHEET_TABLE = "CREATE TABLE IF NOT EXISTS timesheet (taskID INTEGER PRIMARY KEY, resourceID INTEGER PRIMARY KEY, dateSubmitted date, hoursPerTask INTEGER);";
+//		public static final	String CREATE_PROJECT_TABLE = "CREATE TABLE IF NOT EXISTS project (projectID INTEGER PRIMARY KEY, taskID INTEGER, startDate date, endDate date);";
+//		public static final String CREATE_TASK_TABLE = "CREATE TABLE IF NOT EXISTS task (taskID INTEGER PRIMARY KEY, startDate date, endDate date, remainingHours INTEGER);";
+//		public static final String CREATE_CONTAIN_TABLE = "CREATE TABLE IF NOT EXISTS contain (taskID INTEGER PRIMARY KEY, resourceID INTEGER PRIMARY KEY, dateSubmitted date PRIMARY KEY, FOREIGN KEY taskID REFERENCES task(taskID), FOREIGN KEY resouceID REFERENCES resouce(resourceID), FOREIGN KEY dateSubmitted RESOURCES timesheet(dateSubmitted));";
+//		public static final String CREATE_ASSIGNEDTO_TABLE = "CREATE TABLE IF NOT EXISTS assignedTo (taskID INTEGER PRIMARY KEY, resourceID INTEGER PRIMARY KEY, FOREIGN KEY taskID REFERENCES task(taskID), FOREIGN KEY resouceID REFERENCES resouce(resourceID));";
+		
 	}
 	
 	static class MySQL{
-		public static final String CREATE_PERSON_TABLE = Sqlite.CREATE_PERSON_TABLE.replace("AUTOINCREMENT", "AUTO_INCREMENT");
+//		public static final String CREATE_PERSON_TABLE = Sqlite.CREATE_PERSON_TABLE.replace("AUTOINCREMENT", "AUTO_INCREMENT");
 		public static final String CREATE_PERSON_INDEX_LAST_NAME ="";
 		
-		public static final String CREATE_USER_TABLE = Sqlite.CREATE_USER_TABLE.replace("AUTOINCREMENT", "AUTO_INCREMENT");
+//		public static final String CREATE_USER_TABLE = Sqlite.CREATE_USER_TABLE.replace("AUTOINCREMENT", "AUTO_INCREMENT");
+		public static final String CREATE_RESOURCE_TABLE = "CREATE TABLE IF NOT EXISTS resource(resourceID INTEGER, resoureceTeam varchar(25), dateOfBirth date, fName varchar(25), lName varchar(25), PRIMARY KEY (resourceID));";
+		public static final String CREATE_TIMESHEET_TABLE = "CREATE TABLE IF NOT EXISTS timesheet (resourceID INTEGER, dateSubmitted date, hoursPerTask INTEGER, PRIMARY KEY (resourceID, dateSubmitted));";
+		public static final	String CREATE_PROJECT_TABLE = "CREATE TABLE IF NOT EXISTS project (projectID INTEGER PRIMARY KEY, taskID INTEGER, startDate date, endDate date);";
+		public static final String CREATE_TASK_TABLE = "CREATE TABLE IF NOT EXISTS task (taskID INTEGER, startDate date, endDate date, remainingHours INTEGER, PRIMARY KEY (taskID));";
+		public static final String CREATE_CONTAIN_TABLE = "CREATE TABLE IF NOT EXISTS contain (taskID INTEGER, resourceID INTEGER, dateSubmitted date,PRIMARY KEY (taskID, resourceID, dateSubmitted));";
+		public static final String CREATE_ASSIGNEDTO_TABLE = "CREATE TABLE IF NOT EXISTS assignedTo (taskID INTEGER, resourceID INTEGER, PRIMARY KEY (taskID, resourceID));";
+		public static final String ALTER_CONTAIN_TABLE = "ALTER TABLE contain FOREIGN KEY taskID REFERENCES task(taskID) FOREIGN KEY resourceID REFERENCES resource(resourceID) FOREIGN KEY dateSubmitted REFERENCES timesheet(dateSubmitted);";
 		
 	}
+	
 	
     public static final String DROP_PERSON_TABLE = "DROP TABLE IF exists person";    
     public static final String DROP_USER_TABLE = "DROP TABLE IF exists user";
@@ -41,11 +56,19 @@ public class SQLExec {
         statement.setQueryTimeout(10);
         
         if(database.equals("sqlite")){
-            statement.executeUpdate(SQLExec.Sqlite.CREATE_PERSON_TABLE);
-            statement.executeUpdate(SQLExec.Sqlite.CREATE_USER_TABLE);
+ //          statement.executeUpdate(SQLExec.Sqlite.CREATE_PERSON_TABLE);
+//           statement.executeUpdate(SQLExec.Sqlite.CREATE_USER_TABLE);
+         
+            
         }else if(database.equals("mysql")){
-            statement.executeUpdate(SQLExec.MySQL.CREATE_PERSON_TABLE);
-            statement.executeUpdate(SQLExec.MySQL.CREATE_USER_TABLE);
+        	statement.executeUpdate(SQLExec.MySQL.CREATE_RESOURCE_TABLE);
+//            statement.executeUpdate(SQLExec.MySQL.CREATE_PERSON_TABLE);
+//            statement.executeUpdate(SQLExec.MySQL.CREATE_USER_TABLE);
+            statement.executeUpdate(SQLExec.MySQL.CREATE_TASK_TABLE);
+            statement.executeUpdate(SQLExec.MySQL.CREATE_PROJECT_TABLE);
+            statement.executeUpdate(SQLExec.MySQL.CREATE_TIMESHEET_TABLE);            
+            statement.executeUpdate(SQLExec.MySQL.CREATE_CONTAIN_TABLE);
+            statement.executeUpdate(SQLExec.MySQL.CREATE_ASSIGNEDTO_TABLE);
         }
         
         statement.close();
